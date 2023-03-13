55400758436d6a72c09fa7f5941ebc3ee888d3d7
Xylsi <127692204+Xylsi@users.noreply.github.com>
2023-03-13 08:37:35 -0400

    Walkspeed.Lua

---
+++ Walkspeed.lua
@@ -0,0 +1,13 @@
+-- >> Configuration << --
+local isTrueSpeed = 32
+local isFalseSpeed = 16
+local defaultKey = Enum.KeyCode.T -- https://developer.roblox.com/en-us/api-reference/enum/KeyCode
+
+--[[
+    Sources:
+    UIS = https://developer.roblox.com/en-us/api-reference/class/UserInputService
+    Humanoid = https://developer.roblox.com/en-us/api-reference/class/Humanoid
+    isRunning = https://developer.roblox.com/en-us/articles/Boolean
+    isTrueSpeed / isFalseSpeed = https://developer.roblox.com/en-us/articles/Variables
+    defaultKey = https://developer.roblox.com/en-us/api-reference/enum/KeyCode
+--]]
